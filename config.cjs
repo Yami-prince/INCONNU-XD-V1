5b2251592b0d39476d6ef0ba69ce6d14f0fcce3e
Yami-prince <yamistar62@gmail.com>
2025-07-18 23:09:18 -0400

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -4,8 +4,8 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "INCONNU~XD~AR5xjbCZ#DwMzJzPHp2upqjACV0nNuNqyyVkR67ah_JoqiSFTxEE",
-  PREFIX: process.env.PREFIX || '◇',
+  SESSION_ID: process.env.SESSION_ID || "INCONNU~XD~egQDmBRK#Q8HB58MBDbM6cRaCyUqWCkOutP--vHPULEWa9GTZwA8",
+  PREFIX: process.env.PREFIX || '♡',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'false' : false,
   STATUS_READ_MSG: process.env.STATUS_READ_MSG || '',
@@ -21,12 +21,12 @@ const config = {
   
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
-  MODE: process.env.MODE || "public",
+  MODE: process.env.MODE || "Private",
   BOT_NAME: process.env.BOT_NAME || "INCONNU-XD",
   MENU_IMAGE: process.env.MENU_IMAGE || "https://files.catbox.moe/230q0c.jpg",
   DESCRIPTION: process.env.DESCRIPTION || "WELCOME TO INCONNU-XD-V1",
   OWNER_NAME: process.env.OWNER_NAME || " 𝐘𝚫𝚳𝚰",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "18298438605",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "50932391472",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'false' : false, 
 };
